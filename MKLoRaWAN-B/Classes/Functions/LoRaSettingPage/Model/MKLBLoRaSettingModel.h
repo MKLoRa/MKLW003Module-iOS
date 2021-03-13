da2de07dfa19b606fa71841eecc806b7e2851d9e
chengang <aadyx2007@163.com>
2021-03-13 17:29:06 +0800

    完善功能
    Signed-off-by: chengang <aadyx2007@163.com>

--- MKLoRaWAN-B/Classes/Functions/LoRaSettingPage/Model/MKLBLoRaSettingModel.h
+++ MKLoRaWAN-B/Classes/Functions/LoRaSettingPage/Model/MKLBLoRaSettingModel.h
@@ -63,6 +63,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic, assign)NSInteger CHH;
 
+//EU868,CN779, EU433,RU864
 @property (nonatomic, assign)BOOL dutyIsOn;
 
 @property (nonatomic, assign)BOOL adrIsOn;
